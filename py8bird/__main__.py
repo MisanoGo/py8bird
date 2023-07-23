604876e53cf6d5280c635f151cb44d41ba9facf7
misano <misano.go@yahoo.com>
2023-07-23 22:44:48 +0000

    clean up __main__.py

--- py8bird/__main__.py
+++ py8bird/__main__.py
@@ -1,21 +1,4 @@
-from typing import List
-
-import tweepy
-
-from main import TwittePublisher
-import utils
-
-def main():
-    TWITTER_TOKEN: str = utils.env_conf["TWITTER_TOKEN"]
-
-    key_filter: List[str] = [
-        "python",
-    ]
- 
-    tp = TwittePublisher(TWITTER_TOKEN)
-
-    tp.add_rules([tweepy.StreamRule(i) for i in key_filter])
-    tp.filter()
+from main import main
 
 if __name__=="__main__":
     main()
