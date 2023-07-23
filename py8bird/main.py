6fcfa574a18620d52c3608054d552c81cfc6e7d0
misano <misano.go@yahoo.com>
2023-07-23 22:42:27 +0000

    make little

--- py8bird/main.py
+++ py8bird/main.py
@@ -1,70 +1,29 @@
-from typing import List
-from enum import Enum
 import asyncio
 
-from tweepy import StreamingClient, API,OAuth2BearerHandler, StreamRule
-from tweepy.models import Status
-from aiogram import Bot, Dispatcher, Router, F
+from tweepy import StreamingClient
+from aiogram import Bot
 
-from aiogram.utils.keyboard import CallbackData, InlineKeyboardBuilder as IKB
-
-from utils import env_conf
+from utils import env_conf, key_filters
 
 
 BOT_TOKEN: str = env_conf["BOT_TOKEN"]
-CHANNEL_ID: str = env_conf["CHANNEL_ID"]
+OWNER_ID: str = env_conf["OWNER_ID"]
 TWITTER_TOKEN: str = env_conf["TWITTER_TOKEN"]
 
-aiobot: Bot = Bot(BOT_TOKEN)
-router: Router = Router()
+aiobot: Bot = Bot(BOT_TOKEN, parse_mode="HTML")
 
-auth = OAuth2BearerHandler(TWITTER_TOKEN)
-api: API = API(auth)
-
-
-class Action(str, Enum):  
-    cancel: str = "cancel"
-    send: str = "send"
-
-class AdminAction(CallbackData, prefix="adm"):
-    action: Action
-    tweet_id: str
-
-async def getAdminApproval(tweet):
-    b = IKB()
-    for a in Action:
-        b.button(text=a.value.title(),callback_data=AdminAction(action=a,tweet_id=tweet.id))
-    msg: str = f"{tweet.text}\n"
-    await aiobot.send_message(CHANNEL_ID,msg,reply_markup=b.as_markup())
+async def sendTwitte(tweet):
+    await aiobot.send_message(OWNER_ID,tweet.text)
 
 class TwittePublisher(StreamingClient):
     def on_tweet(self, tweet):
-
-        asyncio.run(getAdminApproval(tweet))
-  
-
-@router.callback_query(AdminAction.filter(F.action == Action.send))
-async def publishTwitte(callback_query,callback_data: AdminAction, bot: Bot):
-    api.retweet(callback_data.tweet_id)
-
-    tweet = api.get_status(callback_data.tweet_id)
-    bot.send_message(CHANNEL_ID,tweet)
+        asyncio.run(sendTwiite(tweet))
+ 
 
 def main():
-    key_filter: List[str] = [
-        "python",
-    ]
-    dp = Dispatcher()
-    dp.include_router(router)
-
     tp = TwittePublisher(TWITTER_TOKEN)
-    tp.add_rules([StreamRule(i) for i in key_filter])
+    tp.add_rules([StreamRule(i) for i in key_filters])
     tp.filter()
-
-    asyncio.run(dp.start_polling(aiobot))
-
-
 
 if __name__=="__main__":
     main()
-
--- py8bird/utils.py
+++ py8bird/utils.py
@@ -2,3 +2,16 @@ from dotenv import dotenv_values
 
 # get environment varables from ./.env by dotenv
 env_conf = dotenv_values(verbose=True)
+
+
+def key_filters():
+    """
+    return key filters as listself.
+    TODO: set this function as @property
+
+    """
+    file_name = "./key_filters.list"
+
+    with open(file_name,"r") as file:
+        return file.read().splitlines()
+
